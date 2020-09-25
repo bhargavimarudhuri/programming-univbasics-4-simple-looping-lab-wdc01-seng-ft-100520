39d41d37a6fdb16076d094ff384e948b14d1e9a5
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 05:47:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,7 +6,7 @@ def loop_message_five_times(string)
     end 
 end  
 
-def loop_message_n_times(string, number)
+def loop_message_n_times(message, limit)
   count = 0
   while count < limit do
     puts message
