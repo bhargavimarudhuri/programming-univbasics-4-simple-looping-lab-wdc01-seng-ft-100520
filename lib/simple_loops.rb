e019dbc5585a0ec03fb0107682eb95e973b981ba
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 05:43:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,21 @@
 def loop_message_five_times(string)
-  
+  count = 0
+   while count < 5 do
+     puts string
+     count += 1 
+    end 
 end  
 
 def loop_message_n_times(message_string, integer)
   
 end
 
-def 
+def output_array(array)
+  
+end
+
+def return_string_array(array)
+  
+end  
   
 # Write your methods here
